1ae693782350d5cc9f55ffc0e01f5afcabbde661
david <davidestudosdev@gmail.com>
2024-06-10 15:28:25 -0300

    correção login

--- login_action.php
+++ login_action.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['user_name'] = $user['nome'];
         header("Location: dashboard.php");
     } else {
-        header("Location: login.php");
+        header("Location: index.php");
     }
 }
 ?>
